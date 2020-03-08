ac9916e57af0696967aaea58f79df2fe2a4e5eb7
tt2288 <twtb@kkbr.jp>
2020-03-09 00:46:13 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 'use strict';
+
+const tmp = new Map();
+tmp.set(0, 0);
+tmp.set(1, 1);
+
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
+  if (tmp.has(n)) {
+    return tmp.get(n);
   }
-  return fib(n - 1) + fib(n - 2);
+  const value = fib(n - 1) + fib(n - 2);
+  tmp.set(n, value);
+  return value;
 }
 
 const length = 40;
 for (let i = 0; i <= length; i++) {
   console.log(fib(i));
 }
+// console.log(tmp);
